160a9275452575020ae950f92cd706613dd0c62a
Robert Sebescen <pgrobban@gmail.com>
2015-07-02 17:35:41 +0200

    firefox date fix

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ function getDates(startDate, stopDate) {
     var dateArray = new Array();
     var currentDate = startDate;
     while (currentDate <= stopDate) {
-        dateArray.push(new Date(currentDate))
+        dateArray.push(currentDate)
         currentDate = currentDate.addDays(1);
     }
     return dateArray;
